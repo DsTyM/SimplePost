2f5f5941e2bb6f01fefc04e20614cfba099276f7
DsTyM <stavrosyomafias@gmail.com>
2019-08-24 14:56:20 +0300

    creating test servlet class to test connection pooling

---
+++ src/com/dstym/controller/TestServlet.java
@@ -0,0 +1,56 @@
+package com.dstym.controller;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+
+    // Define datasource/connection pool for Resource Injection
+    @Resource(name="jdbc/simple_post")
+    private DataSource dataSource;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Step 1:  Set up the printwriter
+        PrintWriter out = response.getWriter();
+        response.setContentType("text/plain");
+
+        // Step 2:  Get a connection to the database
+        Connection myConn = null;
+        Statement myStmt = null;
+        ResultSet myRs = null;
+
+        try {
+            myConn = dataSource.getConnection();
+
+            // Step 3:  Create a SQL statements
+            String sql = "select * from users";
+            myStmt = myConn.createStatement();
+
+            // Step 4:  Execute SQL query
+            myRs = myStmt.executeQuery(sql);
+
+            // Step 5:  Process the result set
+            while (myRs.next()) {
+                String username = myRs.getString("username");
+                out.println(username);
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+}
